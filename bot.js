f2d3b93d535ed7b213932badb33c0d7497f892d7
esben holk nielsen <esbenholknielsen@gmail.com>
2021-07-05 18:14:08 +0200

    message handling optimised

--- bot.js
+++ bot.js
@@ -14,10 +14,8 @@ let bot;
 if (process.env.NODE_ENV === 'production') {
    bot = new TelegramBot(token);
    bot.setWebHook(process.env.HEROKU_URL + bot.token);
-   console.log("on production:", process.env.HEROKU_URL + bot.token);
 } else {
    bot = new TelegramBot(token, { polling: true });
-   console.log("not on production:", token);
 
 }
 
@@ -28,11 +26,9 @@ bot.onText(/\/start/, (msg) => {
     msg.chat.id,
     "typing"
   )
-  bot.sendMessage(msg.chat.id, "I am the HRWAILL_Bot, a working prostesis to the HRWAILL ARCHIVE of Human Expressions", {
-    "reply_markup": {
-        "keyboard": [["/MeetMe", "/FeedMe", "/SayGoodbye"]]
-        }
-    });
+  bot.sendMessage(msg.chat.id, "I am the HRWAILL_Bot, a working prostesis to the HRWAILL ARCHIVE of Human Expressions");
+  bot.sendMessage(msg.chat.id, "use /MeetMe, /FeedMe or /SayGoodbye to interact with me");
+
 });
 
 
@@ -44,11 +40,9 @@ bot.onText(/\/MeetMe/, (msg) => {
     "typing"
   )
   bot.sendPhoto(msg.chat.id, "https://res.cloudinary.com/www-houseofkilling-com/image/upload/v1623410193/textures/thumnnail.png_ddeav5.jpg")
-  bot.sendMessage(msg.chat.id, "THE HRWAILL ARCHIVE of Human Expressions is a user generated data set that unfolds into a 3D landscape. On www.stayvirtual.online, you can play as data input for the semi-sentient machine learning algorithm HRWAILL and enjoy First Person Shooter access to its slowly expanding index of signs, images and symbols. Here, you can help the algorithm make sense of its content dreamscapes by digging through your own archives of imagery and sharing them to the void." , {
-    "reply_markup": {
-        "keyboard": [["/FeedMe", "/SayGoodbye"]]
-        }
-    });
+  bot.sendMessage(msg.chat.id, "THE HRWAILL ARCHIVE of Human Expressions is a user generated data set that unfolds into a 3D landscape. On www.stayvirtual.online, you can play as data input for the semi-sentient machine learning algorithm HRWAILL and enjoy First Person Shooter access to its slowly expanding index of signs, images and symbols. Here, you can help the algorithm make sense of its content dreamscapes by digging through your own archives of imagery and sharing them to the void." );
+  bot.sendMessage(msg.chat.id, "Wanna /FeedMe ?" );
+
 });
 
 
@@ -57,7 +51,7 @@ bot.onText(/\/FeedMe/, (msg) => {
 });
 
 bot.onText(/\/SayGoodbye/, (msg) => {
-  bot.sendMessage(msg.chat.id, "see you next time dear human user: " + msg.chat.first_name);
+  bot.sendMessage(msg.chat.id, "see you next time dear human user: " + msg.chat.first_name );
 });
 
 bot.onText(/\/help/, (msg) => {
@@ -67,7 +61,7 @@ bot.onText(/\/help/, (msg) => {
     msg.chat.id,
     "typing"
   )
-  bot.sendMessage(msg.chat.id, "I am THE HRWAILL ARCHIVE of Human Expressions: a user generated data set that unfolds into a 3D landscape. Each image that you give to me, on my website or here in the chat, is rendered into my mind and body as the ever expanding mycelium of meaning production that you and me call cognition.");
+  bot.sendMessage(msg.chat.id, "i usually just go with the flow. Follow instructions and keep it simple: send me an image. make sure its not a movie or a gif. ");
 
 
 
@@ -119,13 +113,14 @@ async function uploadImage(file_id, msg){
 
   bot.sendMessage(msg.chat.id,  answer);
 
-
-  
   if(uploadRes.info.detection.aws_rek_face.data.celebrity_faces[0] && uploadRes.info.detection.aws_rek_face.data.celebrity_faces[0].match_confidence > 80){
     bot.sendMessage(msg.chat.id, 'and I am like maybe ' + uploadRes.info.detection.aws_rek_face.data.celebrity_faces[0].match_confidence + '/100 certain that it also contains ' + uploadRes.info.detection.aws_rek_face.data.celebrity_faces[0].name );
   } else if(uploadRes.info.detection.aws_rek_face.data.celebrity_faces[0] && uploadRes.info.detection.aws_rek_face.data.celebrity_faces[0].match_confidence < 80){
     bot.sendMessage(msg.chat.id, 'and I am pretty unsure about this one, but maybe this is ' + uploadRes.info.detection.aws_rek_face.data.celebrity_faces[0].name +'?');
   }
+
+  bot.sendMessage(msg.chat.id, 'Wanna /FeedMe some more?, or /SayGoodbye already');
+
 
 }
 
