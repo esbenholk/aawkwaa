4c1dee789ae332a09cd6a21dfe5e4e200fc24c6d
esben holk nielsen <esbenholknielsen@gmail.com>
2021-07-05 13:43:52 +0200

    initial commit -deployment test

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 const express = require('express')
 const bodyParser = require('body-parser');
-const parser = require('./parser.js');
 
 require('dotenv').config();
 
@@ -12,34 +11,39 @@ let bot;
 if (process.env.NODE_ENV === 'production') {
    bot = new TelegramBot(token);
    bot.setWebHook(process.env.HEROKU_URL + bot.token);
+   console.log("on production:", process.env.HEROKU_URL + bot.token);
 } else {
    bot = new TelegramBot(token, { polling: true });
+   console.log("not on production:", token);
+
 }
 
-// Matches "/word whatever"
-bot.onText(/\/word (.+)/, (msg, match) => {
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
   const chatId = msg.chat.id;
-  const word = match[1];
-  axios
-    .get(`${process.env.OXFORD_API_URL}/entries/en-gb/${word}`, {
-      params: {
-        fields: 'definitions',
-        strictMatch: 'false'
-      },
-      headers: {
-        app_id: process.env.OXFORD_APP_ID,
-        app_key: process.env.OXFORD_APP_KEY
-      }
-    })
-    .then(response => {
-      const parsedHtml = parser(response.data);
-      bot.sendMessage(chatId, parsedHtml, { parse_mode: 'HTML' });
-    })
-    .catch(error => {
-      const errorText = error.response.status === 404 ? `No definition found for the word: <b>${word}</b>` : `<b>An error occured, please try again later</b>`;
-      bot.sendMessage(chatId, errorText, { parse_mode:'HTML'})
-    });
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
 });
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
+
+bot.on('photo', (msg) => {
+  const chatId = msg.chat.id;
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your photo');
+});
+
 
 const app = express();
 
