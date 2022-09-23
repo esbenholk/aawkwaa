4cefa70e0e054d14aa65352f20b48480a27a444b
esben holk nielsen <esbenholknielsen@gmail.com>
2022-09-23 21:26:53 +0200

    remove check

--- bot.js
+++ bot.js
@@ -607,11 +607,11 @@ bot.on('callback_query', function onCallbackQuery(callbackQuery) {
 });
 
 bot.on('message', async (msg) => {
-    console.log(msg);
+    // console.log(msg);
 
-    if(msg.text && msg.text.charAt(0) == "/" ){
-      return;
-    }
+    // if(msg.text && msg.text.charAt(0) == "/" ){
+    //   return;
+    // }
 
     databaseActions.getUser(msg.chat.id).then(result => {
       if(result.rowCount == 0){
